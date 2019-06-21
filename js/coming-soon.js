c250b13be1c5c57318198ec3da6344f7a3473c31
YeonHoJeong <dusgh2101@naver.com>
2019-06-21 17:11:34 +0900

    시간이 얼마업따!4

--- js/coming-soon.js
+++ js/coming-soon.js
@@ -1,6 +1,57 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  var attendees = {};
+  attendees['박세나'] = 'Bad guy';
+  attendees['이상현'] = 'Bad guy';
+  attendees['이화영'] = 'Bang Bang';
+  attendees['정연호'] = 'Bang Bang';
+  attendees['김재훈'] = 'Beer Pong';
+  attendees['유시현'] = 'Beer Pong';
+  attendees['정하민'] = 'Cocktail lover';
+  attendees['최두리'] = 'Cocktail lover';
+  attendees['Hannah'] = 'Color blind';
+  attendees['임동욱'] = 'Color blind';
+  attendees['나누리'] = 'Do not Puke';
+  attendees['남재현'] = 'Do not Puke';
+  attendees['김준휘'] = 'Dumb boy';
+  attendees['윤수경'] = 'Dumb boy';
+  attendees['김경은'] = 'Fashion terrorist';
+  attendees['남재영'] = 'Fashion terrorist';
+  attendees['송철현'] = 'Finger Princess';
+  attendees['안지은'] = 'Finger Princess';
+  attendees['김양경'] = 'Food fighter';
+  attendees['한상민'] = 'Food fighter';
+  attendees['정혜원'] = 'Genious';
+  attendees['하지민'] = 'Genious';
+  attendees['인형호'] = 'Giant foot';
+  attendees['전다솔'] = 'Giant foot';
+  attendees['백지민'] = 'I am a dancer tonight';
+  attendees['석유진'] = 'I am a dancer tonight';
+  attendees['이정인'] = "I don't give a shit";
+  attendees['이중민'] = "I don't give a shit" ;
+  attendees['Bryanna'] = 'Idiot';
+  attendees['전룡재'] = 'Idiot';
+  attendees['Steve'] = 'Lesketit';
+  attendees['최혜은'] = 'Lesketit';
+  attendees['박승후'] = 'Mamaboy';
+  attendees['박지영'] = 'Mamaboy';
+  attendees['Max'] = 'Prince';
+  attendees['이화진'] = 'Prince';
+  attendees['김태훈'] = 'Shy girl';
+  attendees['양하연'] = 'Shy girl';
+  attendees['이승현'] = 'Fuck Soju';
+  attendees['임수현'] = 'Fuck Soju';
+  attendees['Lina'] = 'Solo';
+  attendees['이혜종'] = 'Solo';
+  attendees['이아름'] = 'Whats up';
+  attendees['정재경'] = 'Whats up';
+  attendees['김영욱'] = 'Youtube Star';
+  attendees['장건우'] = 'Youtube Star';
+
+
+
+
   // No JS
 
 })(jQuery); // End of use strict
